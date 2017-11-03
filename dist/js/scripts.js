ce2aaf2b664083fa457b4f7679c57f93b839fae0
deivis <deividielis@gmail.com>
2017-11-03 12:50:58 +0200

    version: 0.0.1-5

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
  * Project (https://github.com/purtiklis/project)
- * Version: 0.0.1-4
- * Generated on: 2017-11-03 12:50:18
+ * Version: 0.0.1-5
+ * Generated on: 2017-11-03 12:50:58
  * Author: Deivydas Vaseris
  */
 
